00304eae368af2daf6f05026feba1054b7695fca
benchmarkco <110875467+benchmarkco@users.noreply.github.com>
2023-04-30 21:06:11 +0000

    Agrego multipart

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
