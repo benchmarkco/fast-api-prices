126525ed8dca9da06a8aac9dfa46d03280ff262f
benchmarkco <110875467+benchmarkco@users.noreply.github.com>
2023-05-05 03:11:53 +0000

    Modularizo el codigo para que todas las apps funcionen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from fastapi import FastAPI, Request, Form, Response
-from fastapi.responses import RedirectResponse
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 from front.view import router as routerFront
 
@@ -8,9 +6,11 @@ from backend.login.view import router as routerLogin
 from backend.ideas.view import router as routerIdeas
 from backend.downloader.view import router as routerDownloader
 
-import yfinance as yf
 
 app = FastAPI()
 
 app.include_router(routerFront, prefix="/front")
+
 app.include_router(routerLogin, prefix="/login")
+app.include_router(routerIdeas, prefix="/ideas")
+app.include_router(routerDownloader, prefix="/downloader")
